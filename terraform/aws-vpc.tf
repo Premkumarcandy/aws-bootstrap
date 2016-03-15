3ad242f753b4b73821bbba45c1fd2f8f83b89488
Stein Inge Morisbak <steinim@gmail.com>
2016-03-15 16:20:32 +0100

    added instances to elbs

--- terraform/aws-vpc.tf
+++ terraform/aws-vpc.tf
@@ -1,26 +1,27 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "${var.region}"
+  region = "${var.aws_region}"
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "default" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
   enable_dns_support = true
   tags {
-    Name = "vpc"
+    Name = "default vpc"
   }
 }
 
 resource "aws_internet_gateway" "gateway" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
 }
 
 # NAT instance
 
 resource "aws_security_group" "nat" {
-  name = "nat"
-  description = "Allow services from the private subnet through NAT"
+  name = "vpc_nat"
+  description = "Allow traffic to pass from the private subnet to the internet"
   ingress {
     from_port = 80
     to_port = 80
@@ -37,8 +38,15 @@ resource "aws_security_group" "nat" {
      from_port = 22
      to_port = 22
      protocol = "tcp"
-     cidr_blocks = ["${aws_subnet.public-subnet.cidr_block}"]
+     cidr_blocks = ["0.0.0.0/0"]
    }
+   ingress {
+     from_port = -1
+     to_port = -1
+     protocol = "icmp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
    egress {
      from_port = 80
      to_port = 80
@@ -51,21 +59,33 @@ resource "aws_security_group" "nat" {
      protocol = "tcp"
      cidr_blocks = ["0.0.0.0/0"]
   }
-  vpc_id = "${aws_vpc.vpc.id}"
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+  egress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = "${aws_vpc.default.id}"
+  tags {
+    Name = "nat SG"
+  }
 }
 
 resource "aws_instance" "nat" {
   ami = "${var.aws_nat_ami}"
   availability_zone = "${var.availability_zone}"
   instance_type = "${var.instance_type}"
+  key_name = "${aws_key_pair.auth.id}"
   security_groups = ["${aws_security_group.nat.id}"]
   subnet_id = "${aws_subnet.public-subnet.id}"
   associate_public_ip_address = true
   source_dest_check = false
-  connection {
-    user = "ubuntu"
-  }
-  key_name = "${aws_key_pair.auth.id}"
   tags = {
     Name = "nat"
     subnet = "public-subnet"
@@ -81,7 +101,7 @@ resource "aws_eip" "nat" {
 # Public subnets
 
 resource "aws_subnet" "public-subnet" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
   cidr_block = "10.0.0.0/24"
   availability_zone = "${var.availability_zone}"
   tags {
@@ -92,7 +112,7 @@ resource "aws_subnet" "public-subnet" {
 # Routing table for public subnets
 
 resource "aws_route_table" "public-route-table" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.gateway.id}"
@@ -110,8 +130,8 @@ resource "aws_route_table_association" "public-subnet" {
 # Private subnets
 
 resource "aws_subnet" "private-subnet" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.0.1.0/24"
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.private_subnet_cidr}"
   availability_zone = "${var.availability_zone}"
   tags {
     Name = "Private subnet"
@@ -121,7 +141,7 @@ resource "aws_subnet" "private-subnet" {
 # Routing table for private subnets
 
 resource "aws_route_table" "private-route-table" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
   route {
     cidr_block = "0.0.0.0/0"
     instance_id = "${aws_instance.nat.id}"
@@ -147,7 +167,7 @@ resource "aws_key_pair" "auth" {
 
 resource "aws_security_group" "bastion" {
   name      = "bastion"
-  vpc_id      = "${aws_vpc.vpc.id}"
+  vpc_id      = "${aws_vpc.default.id}"
   description = "Bastion security group"
   tags {
     Name = "bastion SG"
@@ -200,7 +220,7 @@ resource "aws_instance" "bastion" {
 resource "aws_security_group" "allow_bastion" {
   name = "allow_bastion_ssh"
   description = "Allow access from bastion host"
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
   ingress {
     from_port = 0
     to_port = 65535
@@ -226,7 +246,7 @@ resource "aws_eip" "bastion" {
 # ACL
 
 #resource "aws_network_acl" "acl" {
-#  vpc_id = "${aws_vpc.vpc.id}"
+#  vpc_id = "${aws_vpc.default.id}"
 #  subnet_ids = [
 #      "${aws_subnet.public-subnet.id}",
 #      "${aws_subnet.private-subnet.id}"
@@ -266,7 +286,7 @@ resource "aws_eip" "bastion" {
 
 resource "aws_security_group" "elb" {
   name      = "app.elb"
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
   description = "Security group for App ELB"
 
   tags = {
@@ -331,6 +351,13 @@ resource "aws_elb" "blue" {
     interval          = 15
     target            = "HTTP:8080/"
   }
+
+  instances = ["${aws_instance.app.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+
 }
 
 resource "aws_elb" "green" {
@@ -361,11 +388,18 @@ resource "aws_elb" "green" {
    interval          = 15
     target            = "HTTP:8080/"
   }
+
+  instances = ["${aws_instance.app.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+
 }
 
 resource "aws_security_group" "app" {
   name      = "app"
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = "${aws_vpc.default.id}"
   description = "Security group for App"
 
   tags = {
@@ -390,7 +424,7 @@ resource "aws_security_group" "app" {
       from_port = -1
       to_port = -1
       protocol = "icmp"
-      cidr_blocks = ["${aws_vpc.vpc.cidr_block}"]
+      cidr_blocks = ["${aws_vpc.default.cidr_block}"]
   }
 
   egress {
@@ -415,21 +449,17 @@ resource "aws_instance" "app" {
   ami = "${var.aws_ubuntu_ami}"
   availability_zone = "${var.availability_zone}"
   instance_type = "${var.instance_type}"
+  key_name = "${aws_key_pair.auth.id}"
   security_groups = ["${aws_security_group.app.id}"]
   subnet_id = "${aws_subnet.private-subnet.id}"
+  source_dest_check = false
   connection {
     user = "ubuntu"
   }
-  key_name = "${aws_key_pair.auth.id}"
   tags = {
     Name = "app"
     subnet = "public-subnet"
     role = "app"
   }
-}
-
-resource "aws_eip" "app" {
-  instance = "${aws_instance.app.id}"
-  vpc = true
 }
 
